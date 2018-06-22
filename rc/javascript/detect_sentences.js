e69e635aa351bb5bf35dd309f9a3cdc39f002f46
Sergey Mironov <ka6ah505@gmail.com>
2018-06-22 22:00:13 +0300

    Добавлена функция, которая ищет заданное кол-во слов правее предполагаемого разделителя.

--- rc/javascript/detect_sentences.js
+++ rc/javascript/detect_sentences.js
@@ -1,7 +1,12 @@
 var signs = ".!?";
 var testtext = "В настоящее время Розничный. бизнес и обеспечивающие процессы находятся под сильным негативным влиянием проблемы работоспособности устройств самообслуживания с функцией рециркуляции.";
+var testtext1 = "Рубим.. Пилим дерево(баобаб) руками, ногами";
 
-// поиск координаты расположения потенциального разднлителя
+/**
+ * Поиск координаты расположения потенциального разднлителя
+ * @param {string} text текст в котором ищем предполагаемые разделители
+ * @return {array} список координат
+*/
 var find_separete = function(text) {
 	var positions = [];
 	var tokens = text.split("");
@@ -13,9 +18,42 @@ var find_separete = function(text) {
 	return positions;
 };
 
-// нахождение тегов с левой стороный от потенциальной точки конца предложения
-var search_left_word = function(text, position, get) {
-
+/**
+ * Oбрезание массива до определенного диапазона
+ *
+ * @param {array} arr массив для вырехания подпоследлвательности из него
+ * @param {number} start позиция начала подпоследовательнсти
+ * @param {number} end позиция конца подпоследовательности
+ * @return {array} получившаяся подполследовательность
+*/
+function cutRange(arr, start, end) {
+  var result = [];
+  for (var i = 0; i < arr.length; i++) {
+    if (i >= start && i <= end) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
 };
 
-console.log(find_separete(testtext));
+/**
+ * Нахождение тегов с левой стороный от потенциальной точки конца предложения
+ * @param {string} text текст для поски слов
+ * @param {number} position позиция с которой отбираются слова
+ * @param {number} get количество возвращаемых слов
+ * @return {array} итоговый массив слов
+*/
+var search_left_word = function(text, position, get) {
+	var m = text.split("");
+	var len = text.split("").length;
+	var tempmass = cutRange(m, position+1, len).join("");
+
+	var tokens = tempmass.split(/(\,)|(\))|(\()|(\;)|(\:)|(\+)|(\№)|(\*)|(\&)|(\@)|(\s)/g);
+	tokens = tokens.filter(function(item) {
+		return item != undefined && item != "";
+	});
+	return cutRange(tokens,0,get-1);
+};
+
+var pos = find_separete(testtext1)[0];
+console.log(search_left_word(testtext1, pos, 3));
